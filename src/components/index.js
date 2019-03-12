574912897610bff373033c993864d306f659bbc1
Camilo Carromeu <camilo@carromeu.com>
2019-03-12 16:58:36 -0400

    First version of CSS.

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+
+// import Vue from 'vue'
+import EmbrapaButton from './Button.vue'
+
+const components = {
+  EmbrapaButton
+}
+/*
+Object.keys(components).forEach(name => {
+  // console.log(JSON.stringify(name))
+  Vue.component(name, components[name])
+})
+*/
+export default components
